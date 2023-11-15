0c0029583b3900b43295825cc34649f05d182968
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-11-15 10:11:48 -0700

    test 1 passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+let ary = []
 
+const removeFromArray = function(arrayContents, removed) {
+    ary = arrayContents.filter((element) => 
+        (element !== removed))
+    console.log(ary)
+    return ary
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
