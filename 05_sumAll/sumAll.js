6925c5f1da157f659d0469cbeb84d7e4ed9c496e
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-11-20 10:05:51 -0700

    does this work like this? math.small working now i think

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,20 +3,18 @@
 // returns error with non-number parameters
 
 const sumAll = function(a,b) {
-    if (((typeof a == "number") && a > 0) && (typeof b == "number" && b > 0)){
-        console.log(typeof a,a, typeof b,b)
-        console.log('yay')
+    if ((typeof a == "number" && a > 0) && (typeof b == "number" && b > 0)) {
+    let small = Math.min(a,b)
+    console.log(small)
+    return "small"
     }
-    // let small = Math.min(arguments)
-    // console.log(small)
-    // return "small"
     else {
-    console.log(typeof a,a, typeof b,b)
+    console.log(typeof a,a, typeof b,b, "gsdf")
         return 'EklRROR'
     }
 ;
 }
 
-sumAll(-1,4)
+sumAll(9,4)
 // Do not edit below this line
 module.exports = sumAll;
