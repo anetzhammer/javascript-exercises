afbe8ce69655863f5b5a4a3d76796ed6cbe00576
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-11-21 11:42:22 -0700

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,3 @@
-// works with larger number first
-// returns error with negative numbers
-// returns error with non-number parameters
-
 const sumAll = function (a, b) {
     if (((typeof a == "number") && (a > 0)) && ((typeof b == "number") && (b > 0))) {
         let small = Math.min(a, b)
@@ -16,10 +12,8 @@ const sumAll = function (a, b) {
     else {
         console.log(typeof a, a, typeof b, b,)
         return 'ERROR'
-    }
-    ;
+    };
 }
-
 // sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
