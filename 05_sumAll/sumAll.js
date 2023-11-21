589aaf3595a26b245df6b4f4e4bf9add7710bc51
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-11-21 09:20:14 -0700

    working .push() functionality

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,19 +2,23 @@
 // returns error with negative numbers
 // returns error with non-number parameters
 
-const sumAll = function(a,b) {
-    if ((typeof a == "number" && a > 0) && (typeof b == "number" && b > 0)) {
-    let small = Math.min(a,b)
-    console.log(small)
-    return "small"
+const sumAll = function (a, b) {
+    if (((typeof a == "number") && (a > 0)) && ((typeof b == "number") && (b > 0))) {
+        let small = Math.min(a, b)
+        let max = Math.max(a, b)
+        let count = []
+             for (let i = small; i <= max; i++) {
+                count.push(i)
+                    }
+                    return console.log((count))
     }
     else {
-    console.log(typeof a,a, typeof b,b, "gsdf")
-        return 'EklRROR'
-    }
+    console.log(typeof a, a, typeof b, b,)
+    return 'RROR'
+}
 ;
 }
 
-sumAll(9,4)
+sumAll(9, 4)
 // Do not edit below this line
 module.exports = sumAll;
