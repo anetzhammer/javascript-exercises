a98800967b6dec9f8b1f4ed9f61f87de6d74ead8
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-11-28 09:35:25 -0700

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftc) {
+  const cel = (ftc - 32) * (5/9)
+  return Math.round(cel * 10) / 10 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctf) {
+  const far = (ctf * (9/5) + 32)
+  return Math.round(far * 10) / 10 
 };
 
 // Do not edit below this line
@@ -9,3 +13,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+convertToCelsius(212)
