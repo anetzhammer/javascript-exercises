f662f1d34c480b10ff68e3209700fb6dca4160e3
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-12-07 09:40:49 -0700

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,6 @@ const reverseString = function(string) {
         return string.split("").reverse().join("")
         
 };
-
+reverseString('howdy')
 // Do not edit below this line
 module.exports = reverseString;
