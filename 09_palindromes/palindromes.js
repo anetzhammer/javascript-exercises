60a57e8006e39f2bbbde9fe6e6fe8ea02c30db33
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-12-07 09:41:36 -0700

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+const replaced = x.replace(/[^a-z0-9]/gi,'').toLowerCase();
+const reversed = replaced.split("").reverse().join("")
+if (reversed === replaced) {
+return true
+}
+else 
+return false
 };
+
+
+palindromes('Animal loots foliated detail of stool lamina.')
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
