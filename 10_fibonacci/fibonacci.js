1f24e2bb9ea7c2be457f92c06ebc8d41fd0ebb37
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-12-08 10:48:54 -0700

    10 don

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+// fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
 
-};
+const fibonacci = function(x) {
+    if (x == 1 ) return 1;
+    if (x < 0 ) return "OOPS";
+    let ary = [0,1]
+    let sum = 0
+    for (let i = 0; i < (x-1); i ++) {
+        sum = ary[ary.length-1] + ary[ary.length-2]
+        ary.push(sum)
+    }
+    return sum
+}
 
+fibonacci(4)
 // Do not edit below this line
 module.exports = fibonacci;
