90c6f3b742dcc8befe9365b45eb5293c781d1c03
alex n <107292884+anetzhammer@users.noreply.github.com>
2023-12-11 15:43:09 -0700

    11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (x) {
+let ary = x.map((book) => book.title)
+return ary
 
+    
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
